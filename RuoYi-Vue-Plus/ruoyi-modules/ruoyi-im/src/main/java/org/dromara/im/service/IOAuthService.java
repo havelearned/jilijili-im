678289acfd80ab8c86c174aad4f40b0c1613095d
Amani <47898108+havelearned@users.noreply.github.com>
2024-03-22 23:17:37 +0800

    Delete RuoYi-Vue-Plus directory
    yes

--- RuoYi-Vue-Plus/ruoyi-modules/ruoyi-im/src/main/java/org/dromara/im/service/IOAuthService.java
+++
@@ -1,9 +0,0 @@
-package org.dromara.im.service;
-
-import org.dromara.common.core.domain.R;
-import org.dromara.im.domain.vo.github.GithubTokenInfo;
-import org.dromara.im.domain.vo.github.GithubUserInfo;
-
-public interface IOAuthService {
-
-}
